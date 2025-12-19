466a5d4a312ba8eff52055c531188e11b585e1c9
kastashenkova <astashenkova.katya@gmail.com>
2025-12-20 01:06:32 +0200

    list practice

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,5 +1,14 @@
 package mate.academy
 
-fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
+const val DOUBLE = 2
+const val MORE_THAN_NUMBER = 25
 
+fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
+    return numbers.map {
+        if (it % 2 != 0) {
+            it * DOUBLE
+        } else {
+            it / DOUBLE
+        }
+    }.filter {it > MORE_THAN_NUMBER}
 }
